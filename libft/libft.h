638fccc6cc21c3082fb3207a4e5a8ae86f523d80
Miguel M <mmarques.eped@google.com>
2021-10-26 11:46:42 +0100

    5th submission, part I - moved to src

--- libft/libft.h
+++ libft/libft.h
@@ -19,8 +19,11 @@ int		ft_isprint(int c);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
+char	*ft_strchr(const char *s, int c);
 size_t	ft_strlcpy(char *destination, const char *source, size_t size);
 size_t	ft_strlen(const char *s);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strrchr(const char *s, int c);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 
---
+++ libft/src/ft_memchr.c
@@ -0,0 +1,12 @@
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*src;
+	size_t			i;
+
+	src = (unsigned char *)s;
+	while (i < n)
+
+	
+}
